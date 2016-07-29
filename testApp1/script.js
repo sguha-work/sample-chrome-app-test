1fec8a6bd4749341e0fb6149f4667494d2da8f4d
sguha-work <sguha1988.life@gmail.com>
2016-07-29 15:02:45 +0530

    WIP

--- testApp1/script.js
+++ testApp1/script.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
+  chrome.app.window.create('intro.html', {
     'outerBounds': {
       'width': 400,
       'height': 500
